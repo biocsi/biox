38c9ea34bd0a1860c3d2f8bdbef58ec107afc8fc
bobo <529063092@qq.com>
2018-11-11 21:03:01 +0800

    mix C and Cpp code, using Kmisc::registerFunctions(), add rev function

---
+++ R/biox-package.R
@@ -0,0 +1,9 @@
+##' biox
+##' 
+##' base bio functions
+##' 
+##' @name biox
+##' @docType package
+##' @useDynLib biox, .registration=TRUE, .fixes = "C"
+##' @seealso \code{\link{html}}
+NULL
---
+++ R/rev.R
@@ -0,0 +1,16 @@
+#' Reverse a Vector of Strings
+#' 
+#' Reverses a vector of 'strings' (a character vector). Not safe for
+#' unicode (UTF-8) characters.
+#' 
+#' 
+#' @export
+#' @param x a character vector.
+#' @examples
+#' x <- c("ABC", "DEF", "GHIJ")
+#' rev(x)
+rev <- function(x) {
+  tmp <- .Call("Cstr_rev", as.character(x))
+  tmp[ is.na(x) ] <- NA
+  return(tmp)
+}
